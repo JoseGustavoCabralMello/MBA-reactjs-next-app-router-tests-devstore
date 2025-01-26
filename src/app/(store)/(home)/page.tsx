8ed091fe3603363f72342df2dba921cd912184b5
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-01-26 19:06:54 -0300

    fixing

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,12 +1,16 @@
 import { api } from '@/data/api'
-import { Product } from '@/data/types/products'
+import { Product } from '@/data/types/product'
 import Image from 'next/image'
 import Link from 'next/link'
+
+/**
+ * Cache & Memoization
+ */
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api('/products/featured', {
     next: {
-      revalidate: 60 * 60,
+      revalidate: 60 * 60, // 1 hour
     },
   })
 
@@ -16,16 +20,16 @@ async function getFeaturedProducts(): Promise<Product[]> {
 }
 
 export default async function Home() {
-  const [highlightedProducts, ...otherProducts] = await getFeaturedProducts()
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
 
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/product/${highlightedProducts.slug}`}
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
-          src={highlightedProducts.image}
+          src={highlightedProduct.image}
           className="group-hover:scale-105 transition-transform duration-500"
           width={920}
           height={920}
@@ -34,9 +38,9 @@ export default async function Home() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">{highlightedProducts.title}</span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            {highlightedProducts.price.toLocaleString('pt-BR', {
+            {highlightedProduct.price.toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
               minimumFractionDigits: 0,
@@ -51,7 +55,7 @@ export default async function Home() {
           <Link
             key={product.id}
             href={`/product/${product.slug}`}
-            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
           >
             <Image
               src={product.image}
@@ -65,7 +69,7 @@ export default async function Home() {
             <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
               <span className="text-sm truncate">{product.title}</span>
               <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-                {highlightedProducts.price.toLocaleString('pt-BR', {
+                {product.price.toLocaleString('pt-BR', {
                   style: 'currency',
                   currency: 'BRL',
                   minimumFractionDigits: 0,
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
+import Link from 'next/link'
 import { Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export function Header() {
   return (
@@ -10,30 +10,28 @@ export function Header() {
           devstore
         </Link>
 
-        <form
-          id="s"
-          className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
-        >
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
           <Search className="w-5 h-5 text-zinc-500" />
 
           <input
             placeholder="Buscar produtos..."
-            className="flex-1 bg-transparent outline-none placeholder:text-zinc-500"
+            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
           />
         </form>
       </div>
+
       <div className="flex items-center gap-4">
-        <div className="flex itmes-center gap-2">
+        <div className="flex items-center gap-2">
           <ShoppingBag className="h-4 w-4" />
           <span className="text-sm">Cart (0)</span>
         </div>
 
-        <div className="w-px h-4 bg-zinc-700" />
+        <div className="w-px h-4 bg-zinc-700"></div>
 
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
           <Image
-            src="https://github.com/josegustavocabralmello.png"
+            src="https://github.com/diego3g.png"
             className="h-6 w-6 rounded-full"
             width={24}
             height={24}
